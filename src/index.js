5581a1c0f9b37bcc8e24a2b9965c39397fb2e1a3
Maxime Côté <mcote@reptile.tech>
2018-04-18 17:06:53 -0400

    redux setup

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ console.log(' --- App Started --- ');
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk"
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from "./rootReducer";
 
 import "./global_styles/global_styles.scss";
 
 import App from "./App";
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>
     , document.getElementById('app'));
