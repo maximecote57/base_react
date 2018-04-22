f296a959efa6b56f6f123cf5e4739d66de7a5af9
Maxime Côté <mcote@reptile.tech>
2018-04-22 17:41:19 -0400

    added multilingual functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import {IntlProvider, addLocaleData} from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import fr from 'react-intl/locale-data/fr';
+import Strings from "./strings.json";
+import Cookies from 'universal-cookie';
 
 import Homepage from "./components/pages/Homepage";
 import Simulator from './components/pages/Simulator';
 
 import "./global_styles/global_styles.scss";
 
-const App = () => (
-    <div>
-        <Route path="/" component={Homepage} exact/>
-        <Route path="/simulator" component={Simulator} />
-    </div>
+const cookies = new Cookies();
 
-)
+addLocaleData(fr);
+addLocaleData(en);
+
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            currentLang: cookies.get('lang') || "en",
+            availableLangs: ["en", "fr"]
+        }
+    }
+
+    updateDocumentTitle = () => {
+        document.title = this.state.currentLang == "en" ? 'Ophelia | Get the best out of your debts' : 'Ophelia | Tirez le meilleur de vos dettes';
+    }
+
+    componentDidMount() {
+        this.updateDocumentTitle();
+    }
+
+    componentDidUpdate() {
+        this.updateDocumentTitle();
+    }
+
+    handleLangChange = (newLang) => {
+        this.setState({
+            currentLang: newLang
+        }, () => cookies.set('lang', newLang))
+    }
+
+    render() {
+        return (
+            <IntlProvider locale={this.state.currentLang} messages={Strings[this.state.currentLang]}>
+                <div>
+                    <Route path="/" exact render={(...props) => <Homepage {...props} currentLang={this.state.currentLang} availableLangs={this.state.availableLangs} onLangChange={this.handleLangChange} />} />
+                    <Route path="/simulator" component={Simulator} />
+                </div>
+            </IntlProvider>
+        );
+    }
+}
 
 export default App;
--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import {FormattedMessage} from 'react-intl';
 
 import logo_ophelia from "../../images/logo_ophelia.svg";
 import drawing_man_piggy_bank from "../../images/drawing_man_piggy_bank.png";
@@ -23,7 +24,6 @@ class Homepage extends React.Component {
     }
 
     render() {
-
         return (
             <section className="main-section">
                 <div className="main-section__top">
@@ -31,21 +31,30 @@ class Homepage extends React.Component {
                         <img className="main-section__logo" src={logo_ophelia} />
                     </div>
                     <div className="main-section__top-right-wrapper">
-                        <a className="link text-uppercase js-lang-toggler" href="javascript:void(0)"></a>
-                        <Link to="/simulator">Simulator</Link>
+                        <Link to="/simulator" className="main-section__link link" >Simulator</Link>
+                        {this.props.availableLangs.map((lang) => {
+                            if(lang == this.props.currentLang) return;
+                            return <a key={lang} className="main-section__link link text-uppercase" href="javascript:void(0)" onClick={this.props.onLangChange.bind(this, lang)}>{lang}</a>
+                        })}
                     </div>
                 </div>
                 <div className="main-section__middle d-flex align-items-center">
                     <div className="row align-items-center no-gutters">
                         <div className="left-col">
-                            <h1 className="main-section__title no-opacity js-main-title">The only debt payment<br /> that thickens your wallet.</h1>
+                            <h1 className="main-section__title no-opacity js-main-title">
+                                <FormattedMessage id="homepage.title" defaultMessage="The only debt payment that thickens your wallet."/>
+                            </h1>
                             <div className="main-section__sub-title-wrapper">
                                 <div className="no-opacity js-sub-title">
                                     <div className="main-section__sub-title-header-wrapper">
-                                        <h2 className="main-section__sub-title-header">Impossible?</h2>
+                                        <h2 className="main-section__sub-title-header">
+                                            <FormattedMessage id="homepage.sub_title_header" defaultMessage="Impossible?"/>
+                                        </h2>
                                     </div>
                                     <div className="main-section__sub-title-wrapper">
-                                        <h3 className="main-section__sub-title">We will convince you of the contrary in a few weeks.</h3>
+                                        <h3 className="main-section__sub-title">
+                                            <FormattedMessage id="homepage.sub_title" defaultMessage="We will convince you of the contrary in a few weeks."/>
+                                        </h3>
                                     </div>
                                 </div>
                             </div>
