81ac04834220bbd4f17ea14aaeaefc88e24d5484
Maxime Côté <mcote@reptile.tech>
2018-04-23 17:09:09 -0400

    added helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import {IntlProvider, addLocaleData} from 'react-intl';
+import {IntlProvider, addLocaleData, FormattedMessage} from 'react-intl';
 import en from 'react-intl/locale-data/en';
 import fr from 'react-intl/locale-data/fr';
 import Strings from "./strings.json";
 import Cookies from 'universal-cookie';
+import {Helmet} from "react-helmet";
 
 import Homepage from "./components/pages/Homepage";
 import Simulator from './components/pages/Simulator';
@@ -28,23 +29,6 @@ class App extends Component {
         }
     }
 
-    updateDocumentTitle = () => {
-        const {intl} = new IntlProvider({
-            locale: this.state.currentLang,
-            messages: Strings[this.state.currentLang],
-        }, {}).getChildContext();
-        document.title = intl.formatMessage({id: "document.title" });
-
-    }
-
-    componentDidMount() {
-        this.updateDocumentTitle();
-    }
-
-    componentDidUpdate() {
-        this.updateDocumentTitle();
-    }
-
     handleLangChange = (newLang) => {
         this.setState({
             currentLang: newLang
@@ -52,9 +36,19 @@ class App extends Component {
     }
 
     render() {
+        const {intl} = new IntlProvider({
+            locale: this.state.currentLang,
+            messages: Strings[this.state.currentLang],
+        }, {}).getChildContext();
+        const documentTitle = intl.formatMessage({id: "document.title" });
         return (
             <IntlProvider locale={this.state.currentLang} messages={Strings[this.state.currentLang]}>
                 <div>
+                    <Helmet>
+                        <meta charSet="utf-8" />
+                        <title>{documentTitle}</title>
+                        <link rel="canonical" href="http://opheliafintech.com" />
+                    </Helmet>
                     <Route path="/" exact render={(...props) => <Homepage {...props} currentLang={this.state.currentLang} availableLangs={this.state.availableLangs} onLangChange={this.handleLangChange} />} />
                     <Route path="/simulator" component={Simulator} />
                 </div>
