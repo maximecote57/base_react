3c582543485b0ab8665cff585cdb9abb742801d2
Maxime Côté <mcote@reptile.tech>
2018-05-04 11:00:24 -0400

    refactoring

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -16,6 +16,8 @@ import { injectIntl, FormattedMessage } from 'react-intl';
 import Pager from "../sections/Pager";
 import Dropdown from "../sections/Dropdown"
 
+import {getDiffBetweenTwoDatesInMinutes} from "../tools/DateHelpers";
+
 import "./_products.scss";
 
 class Products extends React.Component {
@@ -23,10 +25,13 @@ class Products extends React.Component {
     constructor() {
 
         super();
+
         this.isLazyLoadActive = false;
+        this.showFilters = true;
         this.LazyLoadTriggerBottomOffset = 250;
         this.hideFiltersWithNoItems = false;
         this.allowMultipleFilters = true;
+        this.cacheTimeInMinutes = 60;
         this.consumer_key = 'ck_d4eb559b025f8e28c9b5defc99fa7447fad93f53';
         this.consumer_password = 'cs_2c1f842b775ad46c44ced6dbebea174e3068edc7';
         this.baseApiURLProducts = 'https://adriengagnon.com/wp-json/wc/v2/products?per_page=100&consumer_key=' + this.consumer_key + "&consumer_secret=" + this.consumer_password;
@@ -37,6 +42,7 @@ class Products extends React.Component {
 
         this.state = {
             products: [],
+            filteredProducts: [],
             productsCategories: [],
             activeProductsCategories: [],
             categoriesOfLoadedProducts: [],
@@ -79,105 +85,6 @@ class Products extends React.Component {
 
     }
 
-    getCategoriesOfLoadedProducts = (products) => {
-
-        let categoriesInProducts = [];
-
-        products.map((product) => {
-            if(product.categories && product.categories.length > 0) {
-                categoriesInProducts.push(product.categories[0].id)
-            }
-        });
-
-        return [...(new Set(categoriesInProducts))];
-
-    };
-
-    getProductsCategories = () => {
-
-        if(localStorage.getItem('base_react_products_categories')) {
-            const productsFromLocalStorage = JSON.parse(localStorage.getItem('base_react_products_categories'));
-            this.setState({
-                productsCategories: productsFromLocalStorage
-            })
-        }
-        else {
-
-            this.setState({
-                areProductsCategoriesLoading: true
-            });
-
-            fetch(this.baseApiURLProductsCategories)
-                .then(response => response.json())
-                .then((productsCategories) => {
-
-                    localStorage.setItem('base_react_products_categories', JSON.stringify(productsCategories));
-                    this.setState({
-                        productsCategories,
-                        areProductsCategoriesLoading: false
-                    })
-                }
-                )
-        }
-
-    };
-
-    getProducts = () => {
-
-        const _MS_PER_DAY = 1000 * 60 * 60 * 24;
-
-        // a and b are javascript Date objects
-        function dateDiffInMinutes(a, b) {
-
-            const diffMs = (b - a); // milliseconds between now & Christmas
-            const diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
-
-            return diffMins;
-        }
-
-        if(localStorage.getItem('base_react_products') && JSON.parse(localStorage.getItem('base_react_products')).data && JSON.parse(localStorage.getItem('base_react_products')).timestamp) {
-
-            const productsFromLocalStorage = JSON.parse(localStorage.getItem('base_react_products')).data;
-            const productsFromLocalStorageTimestamp = JSON.parse(localStorage.getItem('base_react_products')).timestamp;
-
-            if(this.isLazyLoadActive) {
-                document.addEventListener('scroll', this.handleScroll);
-            }
-
-            this.setState({
-                products: productsFromLocalStorage,
-                categoriesOfLoadedProducts : this.getCategoriesOfLoadedProducts(productsFromLocalStorage)
-            });
-        }
-        else {
-
-            this.setState({
-                areProductsLoading: true
-            });
-
-            fetch(this.baseApiURLProducts)
-                .then((response) => response.json())
-                .then((products) => {
-
-                        if(this.isLazyLoadActive) {
-                            document.addEventListener('scroll', this.handleScroll);
-                        }
-
-                        localStorage.setItem('base_react_products', JSON.stringify({data: [...products], timestamp: new Date()}));
-
-                        this.setState({
-                            products: products,
-                            categoriesOfLoadedProducts : this.getCategoriesOfLoadedProducts(products),
-                            areProductsLoading: false
-                        })
-
-                    }
-                )
-        }
-
-    };
-
-
     componentDidMount = () => {
 
         this.getProducts();
@@ -218,9 +125,14 @@ class Products extends React.Component {
             activeProductsCategories.push(filterId);
         }
 
+        let filteredProducts = this.getFilteredProducts(activeProductsCategories);
+
+        this.reorderProducts(filteredProducts);
+
         this.setState({
             activeProductsCategories,
-            currentOffset: 0
+            currentOffset: 0,
+            filteredProducts
         })
 
     };
@@ -235,7 +147,7 @@ class Products extends React.Component {
 
     };
 
-    handleShowPerPageOption = (showPerPageOption) => {
+    handleClickShowPerPageOption = (showPerPageOption) => {
 
         let newShowPerPageOptions = this.state.showPerPageOptions;
 
@@ -255,6 +167,7 @@ class Products extends React.Component {
     handleClickSortOption = (sortPertPageOption) => {
 
         let newSortPerPropertyOptions = this.state.sortPerPropertyOptions;
+        let products = [...this.state.filteredProducts];
 
         newSortPerPropertyOptions.map((newSortPerPropertyOption) => {
 
@@ -262,17 +175,154 @@ class Products extends React.Component {
 
         });
 
+        this.reorderProducts(products);
+
         this.setState({
-            sortPerPropertyOptions : newSortPerPropertyOptions
+            currentOffset: 0,
+            sortPerPropertyOptions : newSortPerPropertyOptions,
+            filteredProducts: products
         })
 
     };
 
-    getFilteredProducts = () => {
+    reorderProducts = (products) => {
+
+        const currentSort = this.state.sortPerPropertyOptions.find((option) => option.selected).value;
+        let compareFunction = null;
+
+        if(currentSort === "alphabetical-asc") {
+
+            compareFunction = (a, b) => {
+
+                if(a.name < b.name) return -1;
+                if(a.name > b.name) return 1;
+                return 0;
+
+            }
+        }
+        else if(currentSort === "alphabetical-desc") {
+
+            compareFunction = (a, b) => {
+
+                if(a.name < b.name) return 1;
+                if(a.name > b.name) return -1;
+                return 0;
+
+            }
+
+        }
+
+        products.sort(compareFunction);
+
+    }
+
+    getCategoriesOfLoadedProducts = (products) => {
+
+        let categoriesInProducts = [];
+
+        products.map((product) => {
+            if(product.categories && product.categories.length > 0) {
+                categoriesInProducts.push(product.categories[0].id)
+            }
+        });
+
+        return [...(new Set(categoriesInProducts))];
+
+    };
+
+    getProductsCategories = () => {
+
+        if(localStorage.getItem('base_react_products_categories')) {
+            const productsCategoriesFromLocalStorage = JSON.parse(localStorage.getItem('base_react_products_categories'));
+            this.setState({
+                productsCategories: productsCategoriesFromLocalStorage
+            })
+        }
+        else {
+
+            this.setState({
+                areProductsCategoriesLoading: true
+            });
+
+            fetch(this.baseApiURLProductsCategories)
+                .then(response => response.json())
+                .then((productsCategories) => {
+
+                        localStorage.setItem('base_react_products_categories', JSON.stringify(productsCategories));
+                        this.setState({
+                            productsCategories,
+                            areProductsCategoriesLoading: false
+                        })
+                    }
+                )
+        }
+
+    };
+
+    getProducts = () => {
+
+        let localStorageProductsObject = JSON.parse(localStorage.getItem('base_react_products'));
+        let localStorageProducts = null;
+        let localStorageProductsTimestamp = null;
+        let diffInMinutesCachedProducts = null;
+
+        if(localStorageProductsObject) {
+            localStorageProducts = localStorageProductsObject.data;
+            localStorageProductsTimestamp = localStorageProductsObject.timestamp;
+            if(localStorageProductsTimestamp) {
+                diffInMinutesCachedProducts = getDiffBetweenTwoDatesInMinutes(new Date(localStorageProductsTimestamp), new Date());
+            }
+        }
+        if(diffInMinutesCachedProducts !== null && diffInMinutesCachedProducts < this.cacheTimeInMinutes) {
+
+            if(this.isLazyLoadActive) {
+                document.addEventListener('scroll', this.handleScroll);
+            }
+
+            this.reorderProducts(localStorageProducts);
+
+            this.setState({
+                products: localStorageProducts,
+                filteredProducts: localStorageProducts,
+                categoriesOfLoadedProducts : this.getCategoriesOfLoadedProducts(localStorageProducts)
+            });
+        }
+        else {
+
+            this.setState({
+                areProductsLoading: true
+            });
+
+            fetch(this.baseApiURLProducts)
+                .then((response) => response.json())
+                .then((products) => {
+
+                        if(this.isLazyLoadActive) {
+                            document.addEventListener('scroll', this.handleScroll);
+                        }
+
+                        localStorage.setItem('base_react_products', JSON.stringify({data: [...products], timestamp: new Date()}));
+
+                        this.reorderProducts(products);
+
+                        this.setState({
+                            products: products,
+                            filteredProducts: products,
+                            categoriesOfLoadedProducts : this.getCategoriesOfLoadedProducts(products),
+                            areProductsLoading: false
+                        })
+
+                    }
+                )
+        }
+
+    };
+
+    getFilteredProducts = (activeProductsCategories) => {
 
         let filteredProducts = [];
 
-        if(this.state.activeProductsCategories.length === 0) {
+        if(activeProductsCategories.length === 0) {
             filteredProducts = this.state.products;
         }
         else if(this.state.products.length > 0) {
@@ -282,7 +332,7 @@ class Products extends React.Component {
                 if(productCategories.length > 0) {
                     productMainCategoryId = productCategories[0].id;
                 }
-                if(this.state.activeProductsCategories.indexOf(productMainCategoryId) !== -1 ) {
+                if(activeProductsCategories.indexOf(productMainCategoryId) !== -1 ) {
                     filteredProducts.push(product);
                 }
             })
@@ -299,10 +349,10 @@ class Products extends React.Component {
         const categoriesOfLoadedProducts = this.state.categoriesOfLoadedProducts;
         const areProductsLoading = this.state.areProductsLoading;
         const areProductsCategoriesLoading = this.state.areProductsCategoriesLoading;
-        const filteredProducts = this.getFilteredProducts();
+        const filteredProducts = this.state.filteredProducts;
         const isLadyLoadActive = this.isLazyLoadActive;
         const nbOfProductsPerPage = this.state.showPerPageOptions.find((option) => option.selected).value;
-        let currentPageProducts = filteredProducts.slice(this.state.currentOffset, this.state.currentOffset + nbOfProductsPerPage);
+        let currentPageProducts = null;
 
         if(this.isLazyLoadActive) {
             currentPageProducts = filteredProducts.slice(0, this.state.nbOfVisibleProductsLazyLoad);
@@ -325,44 +375,46 @@ class Products extends React.Component {
                         </p>
                     </div>
                     <div className="products__container">
-                        <div className="products__filters-container">
-                            <h2 className="products__filters-title"><FormattedMessage id="generic.filters" default="Filters"/></h2>
-                            {(productsCategories.length === 0 && areProductsCategoriesLoading) &&
-                                <div><FormattedMessage id="products.loading-products-categories" default="Loading products categories"/></div>
-                            }
-                            {(productsCategories.length === 0 && !areProductsCategoriesLoading) &&
-                                <div><FormattedMessage id="products.no-products-categories" default="No products categories."/></div>
-                            }
-                            {(productsCategories && productsCategories.length > 0) &&
-                                productsCategories.map((productsCategory) => {
+                        {this.showFilters &&
+                            <div className="products__filters-container">
+                                <h2 className="products__filters-title"><FormattedMessage id="generic.filters" default="Filters"/></h2>
+                                {(productsCategories.length === 0 && areProductsCategoriesLoading) &&
+                                    <div><FormattedMessage id="products.loading-products-categories" default="Loading products categories"/></div>
+                                }
+                                {(productsCategories.length === 0 && !areProductsCategoriesLoading) &&
+                                    <div><FormattedMessage id="products.no-products-categories" default="No products categories."/></div>
+                                }
+                                {(productsCategories && productsCategories.length > 0) &&
+                                    productsCategories.map((productsCategory) => {
 
-                                    let showFilter = true;
+                                        let showFilter = true;
 
-                                    if(this.hideFiltersWithNoItems) {
-                                        if(currentPageProducts.length !== 0) {
-                                            if(categoriesOfLoadedProducts.length > 0) {
-                                                if(categoriesOfLoadedProducts.indexOf(productsCategory.id) === -1) {
-                                                    showFilter = false;
+                                        if (this.hideFiltersWithNoItems) {
+                                            if (currentPageProducts.length !== 0) {
+                                                if (categoriesOfLoadedProducts.length > 0) {
+                                                    if (categoriesOfLoadedProducts.indexOf(productsCategory.id) === -1) {
+                                                        showFilter = false;
+                                                    }
                                                 }
                                             }
                                         }
-                                    }
 
-                                    if(showFilter) {
-                                        return (
-                                            <div className={"products__filters-item " +  (activeProductsCategories.indexOf(productsCategory.id) !== -1 ? 'is-active' : '')} key={productsCategory.id} onClick={this.handleClickFilter.bind(this, productsCategory.id)}>
-                                                <h3>{productsCategory.name}</h3>
-                                            </div>
-                                        )
-                                    }
-                                })
-                            }
-                        </div>
+                                        if (showFilter) {
+                                            return (
+                                                <div className={"products__filters-item " + (activeProductsCategories.indexOf(productsCategory.id) !== -1 ? 'is-active' : '')} key={productsCategory.id} onClick={this.handleClickFilter.bind(this, productsCategory.id)}>
+                                                    <h3>{productsCategory.name}</h3>
+                                                </div>
+                                            )
+                                        }
+                                    })
+                                }
+                            </div>
+                        }
                         <div className="products__products-container">
                             <div className="products__filters-bar">
                                 {!this.isLazyLoadActive &&
                                     <div className="products__filters-bar-dropdown">
-                                        <Dropdown items={this.state.showPerPageOptions} onClickItem={this.handleShowPerPageOption}/>
+                                        <Dropdown items={this.state.showPerPageOptions} onClickItem={this.handleClickShowPerPageOption}/>
                                     </div>
                                 }
                                 {!this.isLazyLoadActive &&
