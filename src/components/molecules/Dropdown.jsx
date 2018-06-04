a624e7132b2e0351393a25f93221fee65b9ea8ea
Maxime Côté <mcote@reptile.tech>
2018-06-04 15:17:44 -0400

    Revert "added redux integration for dropdowns"
    This reverts commit 6a1e73e99afeb77aa643ad8293d37e4594b8d09a.

--- src/components/molecules/Dropdown.jsx
+++ src/components/molecules/Dropdown.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { openDropdown, closeDropdown } from "../../actions/dropdownActions";
-import { connect } from "react-redux";
 
 import "./_dropdown.scss";
 
@@ -11,25 +9,26 @@ class Dropdown extends React.Component {
         super();
 
         this.state = {
+            isOpened: false,
             selectedItem: null
         }
-
     }
 
     handleClickBtn =  () => {
 
-        this.props.openedDropdownId === this.props.id ? this.props.closeDropdown() : this.props.openDropdown(this.props.id);
+        this.setState({
+            isOpened: !this.state.isOpened
+        })
 
     };
 
     handleClickItem =  (item) => {
 
         this.setState({
+            isOpened: false,
             selectedItem: item
         });
 
-
-        this.props.closeDropdown();
         this.props.onClickItem(item.value);
 
     };
@@ -42,7 +41,7 @@ class Dropdown extends React.Component {
                     {this.state.selectedItem ? this.state.selectedItem.text : this.props.title}
                     <i className="fas fa-chevron-down"></i>
                 </button>
-                <div className={"dropdown__list " + (this.props.openedDropdownId === this.props.id ? 'is-opened' : '')}>
+                <div className={"dropdown__list " + (this.state.isOpened ? 'is-opened' : '')}>
                     {
                         this.props.items.map((item) => {
                             return <a key={item.value} className={"dropdown__item " + (this.state.selectedItem === item ? 'is-selected' : '')} href="javascript:void(0)" onClick={this.handleClickItem.bind(this, item)}>{item.text}</a>
@@ -54,12 +53,5 @@ class Dropdown extends React.Component {
     }
 }
 
-const mapStateToProps = state => ({
 
-    openedDropdownId: state.dropdown.openedDropdownId
-
-});
-
-
-
-export default connect(mapStateToProps, { openDropdown, closeDropdown })(Dropdown);
+export default Dropdown;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import productsReducer from "./productsReducer";
 import productsCategoriesReducer from "./productsCategoriesReducer";
-import dropdownReducer from "./dropdownReducer";
 
 export default combineReducers({
     products: productsReducer,
-    productsCategories: productsCategoriesReducer,
-    dropdown: dropdownReducer
+    productsCategories: productsCategoriesReducer
 });
