693b0dd54ba626f7ceefb6d9d25f05bc400f371f
Maxime Côté <mcote@reptile.tech>
2018-06-11 15:47:02 -0400

    renamed pages to pagesTemplates

---
+++ src/components/pagesTemplates/Homepage/Homepage.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { settings } from "../../../SettingsContext";
+import { injectIntl, FormattedMessage } from 'react-intl';
+import axios from 'axios';
+
+class Homepage extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            pageContent: '',
+            isLoadingInfosFromAPI : true
+        }
+
+
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount - homepage')
+        axios.get(settings.apiUrlPages + '/' + 2)
+            .then(res => {
+
+                const pageInfosFromAPI = res.data;
+
+                this.setState({
+                    pageContent: pageInfosFromAPI.post_content,
+                    isLoadingInfosFromAPI: false
+                })
+            })
+
+    }
+
+    render() {
+        console.log('render -  homepage');
+        return (
+            <div className="contact component">
+                <div className="container">
+                    <div className="page-title">
+                        <h1>
+                            <FormattedMessage id="homepage.title" default="Homepage" />
+                        </h1>
+                    </div>
+                    <div>
+                        {this.state.isLoadingInfosFromAPI &&
+                            <div>Loading infos from API</div>
+                        }
+                        {!this.state.isLoadingInfosFromAPI &&
+                            <div dangerouslySetInnerHTML={{__html: this.state.pageContent}} ></div>
+                        }
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+export default injectIntl(Homepage);
